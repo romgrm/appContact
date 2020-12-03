2eca6072f244347f982e5ce5ba6ebea7b62e50ea
romgrm <romaingreaume@gmail.com>
2020-12-03 22:30:00 +0100

    Init Context Api for global State

---
+++ .history/src/Components/Liste_20201203221024.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Contact from './Contact'
+import { Consumer } from '../Context'
+
+
+// On importe le Consumer qui va permettre au component Liste d'utiliser notre Provider et donc utiliser notre State 
+export default class Liste extends Component {
+
+
+
+    // // Fonction qui permet de delete un contact par rapport à son id rentré en paramètre de la fonction
+    // deleteContactData = (id) => {
+    //     this.setState({
+    //         contacts: this.state.contacts.filter(current => current.id !== id) // on ne garde que les id qui ne sont pas égaux à l'id entré en paramètre, autrement dit l'id 'current'
+    //     })
+    //     // Mon "nouveau contacts" est mtn égal à mon contacts filtré par son id 
+    // }
+
+    ©
+}
